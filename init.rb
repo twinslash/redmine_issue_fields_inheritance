65b7b1d932e19f688cc3629b5632166c8b5a872e
Ilya Kolodnik <ilya.kolodnik@twinslash.com>
2013-04-22 14:25:03 +0300

    initial commit

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :redmine_subtask_fields do
+  name 'Redmine Subtask Fields plugin'
+  author 'Ilya Kolodnik'
+  description 'This plugin changes some fields when creating a subtask'
+  version '0.0.1'
+  author_url 'mailto:ilya.kolodnik@gmail.com'
+end
