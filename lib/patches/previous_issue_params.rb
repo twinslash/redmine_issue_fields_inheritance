bfd95cf6f6dae6304f3ca1992a6342f4bdd4bd22
Ilya Kolodnik <ilya.kolodnik@twinslash.com>
2013-04-24 18:07:25 +0300

    send issue params to create next issue on save&next button click

---
+++ lib/patches/previous_issue_params.rb
@@ -0,0 +1,45 @@
+module PreviousIssueParams
+
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :create, :previous_params
+    end
+  end
+
+  module InstanceMethods
+    def create_with_previous_params
+      if params[:continue]
+        call_hook(:controller_issues_new_before_save, { :params => params, :issue => @issue })
+        @issue.save_attachments(params[:attachments] || (params[:issue] && params[:issue][:uploads]))
+        if @issue.save
+          call_hook(:controller_issues_new_after_save, { :params => params, :issue => @issue})
+          respond_to do |format|
+            format.html {
+              render_attachment_warning_if_needed(@issue)
+              flash[:notice] = l(:notice_issue_successful_create, :id => view_context.link_to("##{@issue.id}", issue_path(@issue), :title => @issue.subject))
+              attrs = {
+                :parent_issue_id => @issue.parent_issue_id,
+                :category_id => @issue.category_id,
+                :assigned_to_id => @issue.assigned_to_id,
+                :priority_id => @issue.priority_id,
+                :fixed_version_id => @issue.fixed_version_id
+              }.reject {|k,v| v.nil?}
+              redirect_to new_project_issue_path(@issue.project, :issue => attrs)
+            }
+            format.api  { render :action => 'show', :status => :created, :location => issue_url(@issue) }
+          end
+        else
+          respond_to do |format|
+            format.html { render :action => 'new' }
+            format.api  { render_validation_errors(@issue) }
+          end
+        end
+      else
+        create_without_previous_params
+      end
+    end
+
+  end
+
+end
